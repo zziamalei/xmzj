a58d8de4223c8c9c87ff6ef45c275ecf470e32c9
RayMa <821718601@qq.com>
2017-05-04 10:52:26 +0800

    初次提交

--- msystem/src/test/java/com/xmzj/frame/BaseServiceTest.java
+++ msystem/src/test/java/com/xmzj/frame/BaseServiceTest.java
@@ -1,6 +1,7 @@
 package com.xmzj.frame;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,5 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(value = SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:config/spring-common.xml"})
 public class BaseServiceTest {
+
+    @Test
+    public void testB(){
+
+    }
 
 }
